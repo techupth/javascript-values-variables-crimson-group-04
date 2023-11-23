dca5c6ced2b75aa19c9f8e170b0f408c232260ae
Nitisit Vithunat <nitisitvithunat@MacBook-Pro-khxng-Nitisit.local>
2023-11-23 14:42:31 +0700

    feat:add assignment value

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,5 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
-
-// Start coding here;
-
-let fahrenheit;
+celsius = celsius * 1.8 + 32;
+let fahrenheit = celsius;
+console.log(fahrenheit);
