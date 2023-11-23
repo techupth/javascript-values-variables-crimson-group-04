e650ee37277bd33459a121924704b991f742e4c9
Marisa Phungbunphra <marisamie30@gmail.com>
2023-11-23 14:02:03 +0700

    feat: add assignment value

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,19 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantNam = "Chef's Table by Chef James";
+const totalPrice = 15000;
+const openRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalPrice = 5000; // จะไม่สามารถ reaasign ได้และไม่แสดงผล
+openRestaurant = 2500; // จะไม่สามารถ reaasign ได้และไม่แสดงผล
+
+console.log(address);
+console.log(totalPrice);
+console.log(openRestaurant);
